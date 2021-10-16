8d95b5ab8097580c4896e1dbd683b85d69d02335
KaylaBolden <kaylabolden4@gmail.com>
2021-10-16 14:05:42 -0700

    s1

--- self-and-cross-join.sql
+++ self-and-cross-join.sql
@@ -1,9 +1,14 @@
 -- Get all pairs of actors that worked together.
 
-select * from sakila.film_actor f1
+select f.title, concat(a1.first_name,' ',a1.last_name) as actor1,
+concat(a2.first_name,' ',a2.last_name) as actor2 from (
+select f1.film_id, f1.actor_id as actor1, f2.actor_id as actor2 from sakila.film_actor f1
 join sakila.film_actor f2 on f1.film_id=f2.film_id and f1.actor_id<>f2.actor_id
 where f1.actor_id<f2.actor_id
-order by f1.film_id;
+order by f1.film_id)s1
+left join sakila.actor a1 on s1.actor1=a1.actor_id
+left join sakila.actor a2 on s1.actor2=a2.actor_id
+left join sakila.film f on s1.film_id=f.film_id;
 
 -- Get all pairs of customers that have rented the same film more than 3 times.
 select  sub1.customer_id as customer1,sub2.customer_id as customer2
@@ -18,7 +23,7 @@ on sub1.film_id=sub2.film_id
 and sub1.customer_id<>sub2.customer_id 
 and sub1.customer_id<sub2.customer_id
 group by sub1.customer_id,sub2.customer_id 
-having filmCount >=3;
+having filmCount >3;
 
 -- Get all possible pairs of actors and films.
 select * from (
